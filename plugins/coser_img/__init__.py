36a5767257723dcb724b2751ccf029f92413f8ce
pye52 <pye52@163.com>
2021-08-03 14:30:20 +0800

    :zap: coser插件注册改为on_regex(现在只响应coser纯指令)

--- plugins/coser_img/__init__.py
+++ plugins/coser_img/__init__.py
@@ -1,20 +1,24 @@
 import httpx
-from nonebot import on_command
+import regex
+from nonebot import on_regex
 from nonebot.log import logger
 from nonebot.adapters.cqhttp import MessageSegment
 from nonebot.typing import T_State
 from nonebot.adapters import Bot, Event
 
-coser = on_command('cos', aliases={'coser', 'COS', 'Cos', 'cOS', 'coS'}, priority=5, block=True)
+
+_reg_pattern = r'^[cC][oO][sS](er)?$'
+coser = on_regex(_reg_pattern, priority=5, block=True)
 
 _rosysun_url = 'http://api.rosysun.cn/cos'
 
 
 @coser.handle()
 async def _(bot: Bot, event: Event, state: T_State):
-    # 只匹配cos纯指令，后续带任何字符都不作处理
-    msg = event.get_plaintext()
-    if len(msg) == 0:
+    # 只匹配cos纯指令(或coser，前三个字母不分大小写)，后续带任何字符都不作处理
+    text = event.get_plaintext()
+    matcher = regex.match(_reg_pattern, text)
+    if matcher is not None:
         message = _img_from_rosysun()
         await coser.finish(message)
 
