2b89c94a6a22ea52ba0f43d5c84c966f72cdc24e
justUndertaker <806792561@qq.com>
2021-08-04 04:30:17 +0800

    :zap:修改引用对象

--- utils/database.py
+++ utils/database.py
@@ -1,24 +1,38 @@
 from configs.pathConfig import DATABASE_PATH
-from .log import logger
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
+from nonebot.log import logger
 
 
 class DBclass():
+
     def __init__(self):
+        '''
+        数据库初始化
+        '''
         db_name = 'data.db'
         db_path = DATABASE_PATH+db_name
-        self.DB = create_engine(f'sqlite:///{db_path}?check_same_thread=False')
-        self.Base = declarative_base()
-        logger.info(f'数据库初始化完成……')
+        db_uri = f'sqlite:///{db_path}?check_same_thread=False'
+        self.DB = create_engine(db_uri)
+        self.Base = declarative_base(bind=self.DB)
+
+    def init(self):
+        self.Base.metadata.create_all()
+        logger.info('数据库初始化成功……')
 
     def get_session(self):
         '''
         返回一个seesion链接，记得关闭
         '''
-        session = scoped_session(sessionmaker(bind=self.DB))
+        session = sessionmaker(bind=self.DB)()
         return session
+
+    def shutdown(self):
+        '''
+        关闭数据库
+        '''
+        pass
 
 
 # 全局数据库对象
