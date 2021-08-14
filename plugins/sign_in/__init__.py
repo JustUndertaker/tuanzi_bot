0867392e2a3e5b98a72b34f5155075afd05163fd
justUndertaker <806792561@qq.com>
2021-08-14 11:41:57 +0800

    :zap:增加更新

--- plugins/sign_in/__init__.py
+++ plugins/sign_in/__init__.py
@@ -1,10 +1,11 @@
 from nonebot.adapters.cqhttp import Bot, GroupMessageEvent, MessageSegment
 from nonebot.adapters.cqhttp.permission import GROUP
 from nonebot import on_regex
+from nonebot.permission import SUPERUSER
 from nonebot.typing import T_State
 from utils.utils import scheduler, get_bot
 
-from .data_source import get_sign_in, reset
+from .data_source import get_sign_in, reset, update_info
 from utils.log import logger
 
 __plugin_name__ = '签到系统'
@@ -17,10 +18,23 @@ sign = on_regex(r"^签到$", permission=GROUP, priority=5, block=True)
 async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
     user_id = event.user_id
     user_name = event.sender.card
+    if user_name == '':
+        user_name = event.sender.nickname
     group_id = event.group_id
 
     msg = await get_sign_in(user_id, group_id, user_name)
     await sign.finish(msg)
+
+update = on_regex(r"^更新信息$", permission=SUPERUSER, priority=5, block=True)
+
+
+@update.handle()
+async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
+    bot = get_bot()
+    group_id = event.group_id
+    member_list = await bot.get_group_member_list(group_id=group_id)
+    msg = await update_info(group_id, member_list)
+    await update.finish(msg)
 
 
 # 定时任务
