380a8a1fbb3ebdcc1cb6aa6f887b56622d05a7b7
pye52 <pye52@163.com>
2021-08-14 09:26:12 +0800

    :zap: 现在whatanime插件将只返回根据搜索结果相似度倒序排列的前三项

--- plugins/whatanime/__init__.py
+++ plugins/whatanime/__init__.py
@@ -27,6 +27,7 @@ async def _(bot: Bot, event: MessageEvent, state: T_State):
 
 @whatanime.got('img_url', prompt='图呢?')
 async def _(bot: Bot, event: MessageEvent, state: T_State):
+    url = state['img_url'].strip()
     await whatanime.send('正在识别...')
     try:
         j = httpx.get(_api, params={'url': url}).json()
@@ -35,17 +36,20 @@ async def _(bot: Bot, event: MessageEvent, state: T_State):
             await whatanime.send(f'错误信息: {error}')
         else:
             result = j.get('result', [])
-            for item in result:
+            # 对识别结果按识别率进行排序
+            sorted_result = sorted(result, key=lambda episode: episode['similarity'], reverse=True)
+            # 只返回前三个识别结果
+            for item in sorted_result[:3]:
                 await whatanime.send(_parse_to_message(item))
     except (RequestError, HTTPStatusError) as httpExc:
         logger.error(f'{_plugin_name}插件访问网络异常: {httpExc}')
-        await whatanime.reject('网络异常')
+        await whatanime.send('网络异常')
     except json.JSONDecodeError as jsonExc:
         logger.error(f'{_plugin_name}插件接口返回数据结构异常: {jsonExc}')
-        await whatanime.reject('返回异常')
+        await whatanime.send('返回异常')
     except Exception as e:
         logger.error(f'{_plugin_name}插件异常: {e}')
-        await whatanime.reject('其余异常')
+        await whatanime.send('其余异常')
     finally:
         await whatanime.finish('识别完毕')
 
