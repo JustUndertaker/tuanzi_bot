c6088164ff4f09a6357f421f828487e4896fe007
justUndertaker <806792561@qq.com>
2021-08-16 21:59:39 +0800

    :loud_sound:修改日志支持

--- utils/log.py
+++ utils/log.py
@@ -1,9 +1,22 @@
-import loguru
+import sys as _sys
+import atexit as _atexit
+from loguru import _defaults
+from loguru._logger import Core as _Core
+from loguru._logger import Logger as _Logger
 from configs.pathConfig import LOG_PATH
+from nonebot.log import default_filter, default_format
 
-logger = loguru.logger
+logger = _Logger(_Core(), None, 0, False, False, False, False, True, None, {})
+
+_atexit.register(logger.remove)
 
 custom_format = "{time:MM-DD HH:MM:SS} [{name}] [{level}] | {message}"
+
+if _defaults.LOGURU_AUTOINIT and _sys.stderr:
+    logger.add(_sys.stderr,
+               filter=default_filter,
+               format=default_format,
+               level='INFO')
 
 logger.add(
     LOG_PATH+"debug/{time:YYYY-MM-DD}.log",
@@ -11,6 +24,7 @@ logger.add(
     retention="10 days",
     level="DEBUG",
     format=custom_format,
+    filter=default_filter,
     encoding="utf-8"
 )
 
@@ -20,6 +34,7 @@ logger.add(
     retention="10 days",
     level="INFO",
     format=custom_format,
+    filter=default_filter,
     encoding="utf-8"
 )
 
@@ -29,5 +44,6 @@ logger.add(
     retention="10 days",
     level="ERROR",
     format=custom_format,
+    filter=default_filter,
     encoding="utf-8"
 )
