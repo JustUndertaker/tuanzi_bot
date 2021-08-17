bd0f36317260e83564819b8a69c3a5e0a32d5451
justUndertaker <806792561@qq.com>
2021-08-18 02:00:03 +0800

    :art:命令格式化

--- utils/database.py
+++ utils/database.py
@@ -3,17 +3,23 @@ from configs.pathConfig import DATABASE_PATH
 from modules.user_info import UserInfo
 from modules.group_info import GroupInfo
 from modules.user_level import UserLevel
+from modules.plugin_info import PluginInfo
+
+from nonebot.log import logger
 
 
-def init():
+def database_init():
     '''
     初始化建表
     '''
+    logger.info('正在注册数据库')
     table_list = [
         UserInfo,
         GroupInfo,
-        UserLevel
+        UserLevel,
+        PluginInfo
     ]
     DB = SqliteDatabase(DATABASE_PATH)
     DB.connect()
     DB.create_tables(table_list)
+    logger.info('数据库注册完成')
