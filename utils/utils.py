c3764d9a05303e8679ac5703a0fa375349f2074b
justUndertaker <806792561@qq.com>
2021-08-18 02:00:17 +0800

    :art:命令格式化

--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,12 @@
-from nonebot import require
 import nonebot
+from nonebot import require
 
+# 全局定时器对象
 scheduler = require('nonebot_plugin_apscheduler').scheduler
 
 
 def get_bot():
+    '''
+    全局获取bot对象
+    '''
     return list(nonebot.get_bots().values())[0]
