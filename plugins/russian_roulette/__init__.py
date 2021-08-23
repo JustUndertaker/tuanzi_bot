2d8148158a1c0f7e20f23d0ba26f2cfbbf54591e
pye52 <pye52@163.com>
2021-08-23 15:47:44 +0800

    :construction: 俄罗斯转盘插件基本玩法逻辑已完成

--- plugins/russian_roulette/__init__.py
+++ plugins/russian_roulette/__init__.py
@@ -1,5 +1,4 @@
-import time
-
+import export as export
 from nonebot import on_command
 from nonebot.adapters import Bot
 from nonebot.adapters.cqhttp import GROUP, GroupMessageEvent, Message, MessageSegment
@@ -7,11 +6,23 @@ from nonebot.typing import T_State
 
 from modules.user_info import UserInfo
 from utils.log import logger
+from .accident import random_accident
 from .data_source import *
 from .sentence import *
 
-_plugin_name = '俄罗斯轮盘'
-__plugin_usage__ = "装弹[金币][at](指定决斗对象，为空则所有群友都可接受决斗)"
+from nonebot.plugin import export
+
+from ..sign_in.config import LUCKY_MAX
+
+export = export()
+export.plugin_name = '俄罗斯轮盘'
+export.plugin_usage = '''俄罗斯轮盘帮助：
+    开启游戏：装弹[金额][at](指定决斗对象，为空则所有群友都可接受决斗)
+        示例：装弹10
+    接受对决：接受对决/拒绝决斗
+    开始对决：开枪（轮流开枪，60秒未开枪另一方可通过该命令进行结算）
+'''
+
 russian_roulette = on_command('俄罗斯轮盘', aliases={'装弹', '俄罗斯转盘'}, permission=GROUP, priority=5, block=True)
 _accept = on_command('接受', aliases={'接受决斗', '接受挑战'}, permission=GROUP, priority=5, block=True)
 _refuse = on_command('拒绝', aliases={'拒绝决斗', '拒绝挑战'}, permission=GROUP, priority=5, block=True)
@@ -26,8 +37,8 @@ async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
     latest_duel = await get_latest_duel(group_id)
     # 若决斗有效
     if latest_duel is not None and latest_duel.active():
-        logger.info(f'当前决斗: {latest_duel}')
         if latest_duel.expired():
+            logger.debug(f'终止超时的决斗: {latest_duel}')
             # 超时后终止上一个决斗
             await duel_end(latest_duel)
         # 若决斗未超时，则发送通知并跳过后续步骤
@@ -40,7 +51,7 @@ async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
 
     message = event.message
     if len(message) < 1:
-        await russian_roulette.finish(f'请按照格式: {__plugin_usage__}')
+        await russian_roulette.finish(f'请按照格式: {export.plugin_usage}')
         return
     # 命令后第一个参数必须为数字，作为赌注
     gold = 0
@@ -51,7 +62,7 @@ async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
             gold = int(message_text)
 
     if gold == 0:
-        await russian_roulette.finish(f'请按照格式: {__plugin_usage__}')
+        await russian_roulette.finish(f'请按照格式: {export.plugin_usage}')
         return
     elif gold < 0:
         await russian_roulette.finish('咋地，决斗完还想倒吸钱啊?')
@@ -66,6 +77,10 @@ async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
 
     # 检测决斗发起人是否有足够的金币
     player1_gold = await UserInfo.get_gold(player1_id, group_id)
+    logger.debug(f'开始一场新的决斗:\n'
+                 f'挑战者: {player1_id}\n'
+                 f'挑战者拥有金币: {player1_gold}\n'
+                 f'赌注: {gold}')
     if player1_gold < gold:
         await russian_roulette.finish(f'请出门左转打工挣够钱再来')
         return
@@ -73,8 +88,13 @@ async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
     if player2_id != -1:
         player2_gold = await UserInfo.get_gold(player2_id, group_id)
         if player2_gold < gold:
+            logger.debug(f'被挑战者{player2_id}所拥有金币不足以支付决斗')
             await russian_roulette.finish(f'你的对手太穷了，他不配和你对战')
             return
+        logger.debug(f'被挑战者: {player2_id}\n'
+                     f'被挑战者拥有金币: {player2_gold}')
+    else:
+        logger.debug('未指定被挑战者')
 
     # 若无指定被决斗者，则所有群员都可响应这场决斗
     if player2_id == -1:
@@ -103,24 +123,29 @@ async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
     latest_duel = await get_latest_duel(group_id)
     # 决斗可能因超时被取消(或根本无发生过任何决斗)
     if latest_duel is None or not latest_duel.can_be_accept():
+        logger.debug(f'当前无可被接受挑战的决斗: {latest_duel}')
         await _accept.finish('当前无任何决斗，你接受个什么劲儿')
         return
     # 若决斗超时则跳过后续步骤(更新其状态)
     if latest_duel.expired():
+        logger.debug(f'决斗已超时，不能被接受了: {latest_duel}')
         await duel_end(latest_duel)
         await _accept.finish('决斗已经超时，请重新发起')
         return
     accept_id = event.user_id
     player1_id = latest_duel.player1_id
     player2_id = latest_duel.player2_id
+    logger.debug(f'[接受]当前决斗: {latest_duel}')
     # 用户是否有资格接受决斗(当前决斗未指定任何人，或接受用户是被决斗者)
     if player2_id == -1 or player2_id == accept_id:
         player2_id = accept_id
         latest_duel.player2_id = player2_id
         player2_gold = await UserInfo.get_gold(player2_id, group_id)
         if player2_gold < latest_duel.wager:
+            logger.debug(f'接受决斗者无足够金币: {player2_gold}')
             await russian_roulette.finish(f'你的金币不足以支付决斗费用，请去打工再来')
             return
+        logger.debug(f'当前决斗被接受: {player2_id}，进入下一阶段')
         # 进入下一阶段
         await duel_accept(latest_duel)
         random_s = random_sentence(accept_challenge)
@@ -138,20 +163,24 @@ async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
     latest_duel = await get_latest_duel(group_id)
     # 决斗可能因超时被取消(或根本无发生过任何决斗)
     if latest_duel is None or not latest_duel.can_be_accept():
+        logger.debug(f'当前无可被拒绝挑战的决斗: {latest_duel}')
         await _refuse.finish('当前无任何决斗，你怂个啥哦')
         return
     # 若决斗超时则跳过后续步骤(更新其状态)
     if latest_duel.expired():
+        logger.debug(f'决斗已超时，不能被拒绝了: {latest_duel}')
         await duel_end(latest_duel)
         await _accept.finish('决斗已经超时了，挺起腰板吧')
         return
     refuse_id = event.user_id
     player1_id = latest_duel.player1_id
     player2_id = latest_duel.player2_id
+    logger.debug(f'[拒绝]当前决斗: {latest_duel}')
     if player2_id == -1:
         await _refuse.finish('这场决斗面向所有人，不用站出来认怂')
         return
     if player2_id == refuse_id:
+        logger.debug('用户{player2_id}拒绝了决斗，更新其状态')
         # 更新决斗状态
         await duel_denied(latest_duel)
         message = Message(f'卑微的{MessageSegment.at(player2_id)}拒绝了应用的{MessageSegment.at(player1_id)}')
@@ -164,12 +193,79 @@ async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
 async def _(bot: Bot, event: GroupMessageEvent, state: T_State):
     group_id = event.group_id
     latest_duel = await get_latest_duel(group_id)
+    # 当前没有决斗或不在决斗状态，直接向用户发出通知消息
     if latest_duel is None or not latest_duel.in_duel():
-        await _shot.finish('射射射，你射个啥呢，没有决斗!')
-        return
-    # 检测命令发送者id是否和当前记录的开枪人一致
-    shot_player_id = event.user_id
-    if shot_player_id != latest_duel.in_turn:
-        await _shot.finish(f'枪不在你手上，别捣乱')
+        logger.debug('[开枪]当前无进行中的决斗')
+        await _shot.finish('射射射，你射个啥呢，现在没有任何决斗!')
         return
 
+    shot_player_id = event.user_id
+    another_player_id = latest_duel.another
+    logger.debug(f'[开枪{shot_player_id}]当前决斗: {latest_duel}')
+    # 决斗超时进入结算(由另一方发送开枪才允许触发结算)
+    if shot_player_id == another_player_id and latest_duel.expired():
+        await duel_end(latest_duel)
+        # 进入结算状态
+        winner, loser = latest_duel.clearing()
+        if winner is None or loser is None:
+            # 无人获胜
+            message = MessageSegment.text('竟然忘记装子弹了，这一次无人获胜!')
+        else:
+            message = await _end_of_game(event, latest_duel, winner, loser)
+        logger.debug(f'决斗超时，由另一方发起结算: {another_player_id}')
+        await russian_roulette.finish(message)
+        return
+
+    # 检测命令发送者id是否和当前记录的开枪人一致
+    if shot_player_id != latest_duel.in_turn:
+        await _shot.finish('枪不在你手上，别捣乱')
+        return
+    # 根据开枪用户当天运气，触发额外事件
+    user_fortune = UserInfo.get_lucky(shot_player_id, group_id)
+    if user_fortune is None:
+        user_fortune = 0
+    # 总概率为用户最大运气值的3%(这里强关联了用户的最大运气值)
+    real_fortune = (LUCKY_MAX - user_fortune) * 3
+    r = random.uniform(0, 1) * real_fortune
+    t = random.randint(0, LUCKY_MAX * 100)
+    if t < r:
+        # 触发意外事件，当前子弹直接换人
+        message, shot, end, winner, loser = random_accident(shot_player_id, another_player_id)
+        logger.debug(f'用户触发意外事件:\n'
+                     f'终结消息: {message}\n,'
+                     f'子弹是否射出: {shot}\n,'
+                     f'是否结束事件: {end}\n'
+                     f'胜者: {winner}\n'
+                     f'败者: {loser}')
+        # 是否需要结束决斗
+        if end:
+            end_message = await _end_of_game(event, latest_duel, winner, loser)
+            await russian_roulette.send(message)
+            await duel_end(latest_duel)
+            await russian_roulette.finish(end_message)
+            return
+        # 当前子弹是否已发射
+        if shot:
+            await duel_shot(latest_duel)
+        else:
+            await duel_switch(latest_duel)
+        await russian_roulette.finish(message)
+        return
+    get_shot = await duel_shot(latest_duel)
+    if get_shot:
+        logger.debug(f'用户{shot_player_id}中弹，进入结算')
+        # 中枪后进入结算
+        message = await _end_of_game(event, latest_duel, another_player_id, shot_player_id)
+        await duel_end(latest_duel)
+        await russian_roulette.finish(message)
+
+
+async def _end_of_game(event: GroupMessageEvent, duel: DuelHistory, winner: int, loser: int) -> MessageSegment:
+    group_id = event.group_id
+    wager = duel.wager
+    await UserInfo.change_gold(winner, group_id, wager)
+    await UserInfo.change_gold(loser, group_id, -wager)
+    return MessageSegment.text(
+        f'胜者{MessageSegment.at(winner)}赢得了{wager}枚金币\n'
+        f'败者{MessageSegment.at(loser)}被丢进了海里喂鱼\n'
+        f'子弹: {duel.visual_bullet}')
--- plugins/russian_roulette/data_source.py
+++ plugins/russian_roulette/data_source.py
@@ -25,11 +25,13 @@ class DuelHistory(Model):
     group_id = IntegerField(verbose_name='QQ群号', null=False)
     player1_id = IntegerField(verbose_name='挑战者', default='')
     player2_id = IntegerField(verbose_name='被挑战者', default=0)
+    wager = IntegerField(verbose_name='赌注', default=0)
+    state = IntegerField(verbose_name='状态', default=0)
     in_turn = IntegerField(verbose_name='裁决者')
     # 为啥要用字符串来记录，因为python的二进制处理实在太蛋疼了
     bullet = CharField(verbose_name='弹闸', default=0)
-    wager = IntegerField(verbose_name='赌注', default=0)
-    state = IntegerField(verbose_name='状态', default=0)
+    order = IntegerField(verbose_name='子弹', default=0)
+    probability = IntegerField(verbose_name='中枪概率', default=0)
     last_shot_time = IntegerField(verbose_name='上次开枪时间', default=0)
     start_time = DateTimeField(verbose_name='累计签到次数', default=datetime.datetime.now())
     end_time = DateTimeField(verbose_name='上次签到日期', null=True)
@@ -46,7 +48,7 @@ class DuelHistory(Model):
 
     def active(self) -> bool:
         """
-        决斗是否有效
+        决斗是否有效，有效意味着可以进行"接受"、"拒绝"等操作
         """
         return self.state == 0 | self.state == 1
 
@@ -57,18 +59,32 @@ class DuelHistory(Model):
         return self.state == 1
 
     @property
-    def order(self):
-        order_str = str(self.bullet).split(',')[0]
-        return int(order_str)
-
-    @property
     def current_bullet(self) -> bool:
         """
         获取当前弹闸中子弹，True为有子弹，False为空枪
         """
+        order = int(self.order)
         bullet_str = str(self.bullet).split(',')
-        order = int(bullet_str[0])
         return bullet_str[order] == '1'
+
+    @property
+    def visual_bullet(self) -> str:
+        return str(self.bullet).replace('0', '_')
+
+    def clearing(self):
+        """
+        结算，根据当前状态返回最后(胜者, 败者)id
+        """
+        temp_order = self.order
+        temp_shot_id = self.in_turn
+        bullet_str = str(self.bullet).split(',')[temp_order:]
+        for bullet in bullet_str:
+            if bullet == '1':
+                loser = temp_shot_id
+                winner = self.player2_id if loser == self.player1_id else self.player1_id
+                return winner, loser
+            temp_shot_id = self.player1_id if temp_shot_id == self.player1_id else self.player2_id
+        return None, None
 
     def can_be_shot(self):
         return self.order < 8
@@ -81,6 +97,16 @@ class DuelHistory(Model):
             return (current_time - self.last_shot_time) > _TIMEOUT
         else:
             return True
+
+    @property
+    def another(self):
+        """
+        返回当前开枪的另一位玩家id，如当前轮到player1开枪，则返回player2的id
+        """
+        return self.player1_id if self.in_turn == self.player2_id else self.player2_id
+
+    def switch(self):
+        self.in_turn = self.player1_id if self.in_turn == self.player1_id else self.player2_id
 
     def _state_to_string(self):
         if self.state == 0:
@@ -98,10 +124,15 @@ class DuelHistory(Model):
         return f'groupId: {self.group_id}\n' \
                f'player1Id: {self.player1_id}\n' \
                f'player2Id: {self.player2_id}\n' \
-               f'wager: {self.wager}\n' \
-               f'state: {self._state_to_string()}\n' \
-               f'startTime: {self.start_time}\n' \
-               f'startTime: {self.end_time}\n'
+               f'赌注: {self.wager}\n' \
+               f'决斗状态: {self._state_to_string()}\n' \
+               f'当前开枪用户: {self.in_turn}\n' \
+               f'子弹可视图: {self.visual_bullet}\n' \
+               f'第几颗子弹: {self.order}\n' \
+               f'中枪概率: {self.probability}\n' \
+               f'上次开枪时间: {self.last_shot_time}\n' \
+               f'开始时间: {self.start_time}\n' \
+               f'结束时间: {self.end_time}\n'
 
 
 async def get_latest_duel(group_id: int) -> Union[DuelHistory, None]:
@@ -153,11 +184,12 @@ async def duel_accept(duel: DuelHistory):
     p = random.uniform(0, 1)
     # 生成弹闸列表
     bullet_list = ['1' if r > p else '0' for r in [random.uniform(0, 1) for _ in range(0, 7)]]
-    bullet_list.insert(0, '1')
     # 默认是player1开首枪
     duel.in_turn = duel.player1_id
     duel.bullet = ','.join(bullet_list)
+    duel.probability = p
     duel.state = 1
+    duel.last_shot_time = time.time()
     await DuelHistory.update(duel)
 
 
@@ -172,5 +204,20 @@ async def duel_denied(duel: DuelHistory):
     await DuelHistory.update(duel)
 
 
-async def duel_shot(duel: DuelHistory):
-    order = duel.order
+async def duel_shot(duel: DuelHistory) -> bool:
+    """
+    开枪，同时更换开枪选手
+    True -> 被击中
+    False -> 未被击中
+    """
+    duel.order = duel.order + 1
+    duel.switch()
+    duel.last_shot_time = time.time()
+    await DuelHistory.update(duel)
+    return duel.current_bullet
+
+
+async def duel_switch(duel: DuelHistory):
+    duel.switch()
+    duel.last_shot_time = time.time()
+    await DuelHistory.update(duel)
